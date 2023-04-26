5ef98726ef1f34c8487c4925cf2438409ed58093
Smaguy <68536242+PierreSmague@users.noreply.github.com>
2023-04-26 14:13:11 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from cartes import *
 
 mon_plateau = Plateau("Mage", "Smaguy", "Chasseur", "KamionBen")
 i = 0
-while i < 30:
+while i < 100:
+    ## On choisit des cartes au hasard à jouer, ou pour attaquer
     carte_a_jouer = random.choice(all_cartes)
     if mon_plateau.tour_de_jeu == 1:
         attaquant = random.choice(["heros"] + mon_plateau.serviteurs_joueur1)

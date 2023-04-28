3a422a41fc7528393f08463c55be4284eccca4a4
KamionBen <kamionben@gmail.com>
2023-04-28 16:38:40 +0200

    Fancy devient fancy !

--- fancyinterface.py
+++ fancyinterface.py
@@ -1,19 +1,22 @@
 import pandas.core.frame
 
 from Entities import *
-from random import choice
+from time import sleep
 import pickle
+from init_variables import *
 
-def center_text(text: str, length: int, filler=' ', bold=False) -> str:
+def center_text(text: str, length: int, filler=' ', bold=False, color=None) -> str:
     left = (length - len(text)) // 2
     right = length - left - len(text)
     if bold:
         text = f"\033[1m{text}\033[0m"
+    if color is not None:
+        text = f"{color}{text}\033[0m"
     centered_text = filler * left + text + filler * right
     return centered_text
 
 
-def fancy_c(fancy_card: Card, active=False) -> str:
+def fancy_card(fancy_card: Card, active=False) -> str:
     """ Return a str representing a single card """
     if active:
         a, e = '\033[92m', '\033[0m'  # GREEN, ENDC
@@ -63,11 +66,11 @@ def fancy_c(fancy_card: Card, active=False) -> str:
     return f_card
 
 
-def fancy_l(cartes: list[Card], selection=False, mana=0) -> str:
+def fancy_cardlist(cartes: list[Card], selection=False, mana=0) -> str:
     """ Return a str representing a card hand """
     card_ls = []
     for card in cartes:
-        card_ls.append(fancy_c(card, card.cost <= mana))
+        card_ls.append(fancy_card(card, card.cost <= mana))
     cards_l = [c.split('\n') for c in card_ls]
     cards_dict = {}
     for card in cards_l:
@@ -87,11 +90,58 @@ def fancy_l(cartes: list[Card], selection=False, mana=0) -> str:
     return cards_str
 
 
-def fancy_j(hero) -> str:
+def fancy_hero(hero: Hero, active=False, playing=False) -> str:
     """ Besoin d'une classe "Joueur" ou Héros """
-    j_str = f"┌───────{hero.name}───────┐"
+    if active:
+        a, e = "", ""
+    else:
+        a, e = "", ""
+
+    length = 25
+    yellow = '\033[93m'
+    if playing:
+        j_str = f"┌{center_text(f' {hero.name} ', length-2, bold=True, filler='─', color=yellow)}┐\n"
+    else:
+        j_str = f"┌{center_text(f' {hero.name} ', length - 2, bold=True, filler='─')}┐\n"
+
+    j_str += f"{a}│" + " " * (length - 2) + f"│{e}\n"
+    if hero.attack > 0:
+        pass
+    else:
+        if hero.health < 10:
+            health = f" {hero.health}"
+        else:
+            health = str(hero.health)
+        j_str += "└" + "─" * (length - 9) + f" H={health} ─┘"
 
     return j_str
+
+
+def fancy_mana(mana, mana_max) -> str:
+    symbol_a = "♦︎"
+    symbol_i = "♢"
+    empty = mana_max - mana
+    active = [symbol_a for _ in range(mana)]
+    incative = [symbol_i for _ in range(empty)]
+    return f"[{mana}/{mana_max}] "+" ".join(active) +" "+ " ".join(incative)
+
+
+class FancyLog:
+    def __init__(self):
+        self.log = ["" for _ in range(10)]
+
+    def add(self, action: str):
+        self.log.insert(0, action)
+
+    def __iter__(self):
+        return iter(self.log)
+
+    def get(self, nb=10):
+        return self.log[:nb]
+
+    def print(self, index, whitespace=50):
+        return self.log[index] + " " * (whitespace - len(self.log[index]))
+
 
 
 def import_log(file: str) -> pandas.core.frame.DataFrame:
@@ -100,21 +150,54 @@ def import_log(file: str) -> pandas.core.frame.DataFrame:
 
 
 if __name__ == '__main__':
-    player = Player("KamionBen")
-    player.set_deck("Chasseur", "basic_chasseur.csv")
-    player.start_game()
-    player.start_turn()
-    print(fancy_l(player.hand, mana=player.mana))
+    fancylog = FancyLog()
     log = import_log("modelisation/logs_games.pickle")
-    for elt in log.values:
-        player = elt[71]
-        action = elt[0]
+    sample = {columns_logs[i]: elt for i, elt in enumerate(log.values[0])}
 
-        print([f"{i} : {a}" for i, a in enumerate(elt)])
-        if action == "passer_tour":
-            print(f"{player} passe son tour.")
-        elif action == "jouer_carte":
-            card = elt[1] if elt[1] != "" else elt[2]
-            print(f"{player} joue {card}")
+    players = [Player(sample["pseudo_j"], sample["classe_j"]), Player(sample["pseudo_adv"], sample["classe_adv"])]
+    players[0].set_deck("test_deck.csv")
+    players[1].set_deck("test_deck.csv")
+
+    turn = 1
+    side = 0
+
+    for line in log.values[:10]:
+        event = {columns_logs[i]: elt for i, elt in enumerate(line)}
+        if event["action"] == "passer_tour":
+            fancylog.add(f"{players[side].name} passe son tour.")
+            if side == 0:
+                side = 1
+            else:
+                side = 0
+                turn += 1
+        if event["action"] == "jouer_carte":
+            fancylog.add(f"{players[side].name} joue {event['carte_jouee']}")
+
+        players[0].mana, players[0].mana_max = event["mana_dispo_j"], event["mana_max_j"]
+        players[0].hero.health = event["pv_j"]
+
+        players[1].mana, players[1].mana_max = event["mana_max_adv"], event["mana_max_adv"]
+        players[1].hero.health = event["pv_adv"]
+
+        print("\n" * 50)
+        print(event)
+        log_width = 50
+
+        print(fancylog.print(0) + f"Tour {turn}")
+        print(fancylog.print(1) + fancy_mana(players[1].mana, players[1].mana_max))
+        adv = fancy_hero(players[1].hero, playing=side == 1).split('\n')
+        print(fancylog.print(2) + adv[0])
+        print(fancylog.print(3) + adv[1])
+        print(fancylog.print(4) + adv[2])
+        print(fancylog.print(5))
+        print(fancylog.print(6))
+        print(fancylog.print(7))
+        print(fancylog.print(8))
+        jr = fancy_hero(players[0].hero, playing=side == 0).split("\n")
+        print(fancylog.print(9) + jr[0])
+        print(" "*log_width + jr[1])
+        print(" "*log_width + jr[2])
+        print(" "*log_width + fancy_mana(players[0].mana, players[0].mana_max))
+        sleep(2)
 
 

3b805069692bdd25b8e3a10d3c32c29a09cdfe13
smaguy <psmague@gmail.com>
2023-05-16 08:19:08 +0200

    Nouveau modèle + migration engine/entities

--- modelisation/model_with_mask.py
+++ modelisation/model_with_mask.py
@@ -1,26 +1,19 @@
-import numpy as np
-import random
-import pickle
 import matplotlib.pyplot as plt
-from tf_agents.trajectories import time_step as ts
-import tensorflow as tf
 from tf_agents.agents.dqn import dqn_agent
-from tf_agents.specs import array_spec
+from tf_agents.agents.categorical_dqn import categorical_dqn_agent
 from tf_agents.replay_buffers import tf_uniform_replay_buffer
-from tf_agents.environments import py_environment, tf_py_environment
 from tf_agents.networks import sequential
 from tf_agents.policies import random_tf_policy, q_policy, PolicySaver
 from tf_agents.trajectories import trajectory
 from tf_agents.specs import tensor_spec
 from tf_agents.utils import common
-from sklearn.model_selection import train_test_split
-from tf_agents.utils.nest_utils import batch_nested_tensors, is_batched_nested_tensors
+from modelisation.engine import *
 
 
 
 """ Chargement des données d'entraînement et de données d'init """
-with open('logs_refined.pickle', 'rb') as f:
-    df_state = pickle.load(f)
+# with open('logs_refined.pickle', 'rb') as f:
+#     df_state = pickle.load(f)
 
 
 def generate_legal_vector(state):
@@ -31,8 +24,7 @@ def generate_legal_vector(state):
 
     """ Peut-on jouer une carte ? """
     for i in range(int(state["nbre_cartes_j"])):
-        if state[f"carte_en_main{i + 1}_cost"] <= state["mana_dispo_j"] and state[
-            f"carte_en_main{i + 1}_cost"] != 99:
+        if state[f"carte_en_main{i + 1}_cost"] <= state["mana_dispo_j"] and state[f"carte_en_main{i + 1}_cost"] != -99:
             legal_actions[1] = True
             break
 
@@ -45,15 +37,77 @@ def generate_legal_vector(state):
                     legal_actions[2 + 8 * i + j] = True
     return legal_actions
 
+
+def estimated_advantage(action, state):
+    """ Simule le plateau qu'aurait donné une certaine action pour en tirer une notion d'avantage gagné ou perdu """
+    card_advantage = 0
+    board_advantage = 0
+    health_advantage = 0
+
+    if action == 0:
+        card_advantage -= 1
+    elif action == 1:
+        card_advantage -= 1
+        j = 0
+        for i in range(7):
+            if state[f"carte_en_main{i+1}_cost"] <= state["mana_dispo_j"] and state[f"carte_en_main{i+1}_cost"] != -99:
+                board_advantage += state[f"carte_en_main{i+1}_atk"]
+                board_advantage += state[f"carte_en_main{i+1}_pv"]
+                j += 1
+        board_advantage = board_advantage / max(j, 1)
+    else:
+        attacker = state[f"atq_serv{(action - 2) // 8}_j"], state[f"pv_serv{(action - 2) // 8}_j"]
+        if (action - 2) % 8 == 0:
+            if state["pv_adv"] - attacker[0] <= 0:
+                return 1
+            else:
+                health_advantage += attacker[0]
+        else:
+            defender = state[f"atq_serv{(action - 2) % 8}_adv"], state[f"pv_serv{(action - 2) % 8}_adv"]
+            if attacker[0] >= defender[1]:
+                if defender[0] >= attacker[1]:
+                    board_advantage += defender[0] + defender[1] - attacker[0] - attacker[1]
+                else:
+                    board_advantage += defender[1]
+            else:
+                if defender[0] >= attacker[1]:
+                    board_advantage += attacker[0] - attacker[1]
+                else:
+                    board_advantage += attacker[0] - defender[0]
+    coef_cards, coef_board, coef_health = 0.5, 1, 1
+    return 0
+
+
+players = [Player("IA1", "Mage"), Player("IA2", "Chasseur")]
+plateau_depart = Plateau(players)
+
+columns_actual_state = ["mana_dispo_j", "mana_max_j",
+                "mana_max_adv", "surcharge_j", "surcharge_adv", "pv_j", "pv_adv", "pv_max_j", "pv_max_adv", "nbre_cartes_j",
+                "nbre_cartes_adv", "action", "victoire"]
+
+for i in range(10):
+    columns_actual_state.append(f"carte_en_main{i + 1}_cost")
+    columns_actual_state.append(f"carte_en_main{i + 1}_atk")
+    columns_actual_state.append(f"carte_en_main{i + 1}_pv")
+
+for i in range(7):
+    columns_actual_state.append(f"atq_serv{i + 1}_j")
+    columns_actual_state.append(f"pv_serv{i + 1}_j")
+    columns_actual_state.append(f"atq_remain_serv{i + 1}_j")
+
+for i in range(7):
+    columns_actual_state.append(f"atq_serv{i + 1}_adv")
+    columns_actual_state.append(f"pv_serv{i + 1}_adv")
+
+
 class Frenchstone(py_environment.PyEnvironment):
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
         self._action_spec = array_spec.BoundedArraySpec(shape=(), dtype=np.int32, minimum=0, maximum=65, name='action')
+        self._state = plateau_depart
         self._observation_spec = {
-            'observation': array_spec.BoundedArraySpec(shape=(self.data.shape[1],), dtype=np.int32, minimum=-100, maximum=100, name='observation'),
+            'observation': array_spec.BoundedArraySpec(shape=(len(itemgetter(*columns_actual_state)(self._state.get_gamestate())),), dtype=np.int32, minimum=-100, maximum=100, name='observation'),
             'valid_actions': array_spec.ArraySpec(name="valid_actions", shape=(66,), dtype=np.bool_)
         }
-        self._state = self.data.loc[random.randint(0, self.data.shape[0] - 1)]
         self._episode_ended = False
 
     def action_spec(self):
@@ -63,92 +117,86 @@ class Frenchstone(py_environment.PyEnvironment):
         return self._observation_spec
 
     def _reset(self):
-        self._state = self.data.loc[random.randint(0, self.data.shape[0] - 1)]
+        if bool(random.getrandbits(1)):
+            self._state = Plateau([Player("IA1", "Mage"), Player("IA2", "Chasseur")])
+        else:
+            self._state = Plateau([Player("IA2", "Chasseur"), Player("IA1", "Mage")])
+            while self._state.get_gamestate()['pseudo_j'] != 'IA1':
+                self._state = Orchestrator().tour_oldia_from_training(self._state, old_policy, oldpolicy_state)
         self._episode_ended = False
         obs = self.observation_spec()
 
         """ Gestion des actions légales """
-        legal_actions = generate_legal_vector(self._state)
+        legal_actions = generate_legal_vector(self._state.get_gamestate())
 
-        obs['observation'] = np.array(self._state, dtype=np.int32)
+        obs['observation'] = np.array(itemgetter(*columns_actual_state)(self._state.get_gamestate()))
         obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
         return ts.restart(obs)
 
     def _step(self, action):
-
-        self._state = self.data.loc[random.randint(0, self.data.shape[0] - 1)]
 
         if self._episode_ended:
             # The last action ended the episode. Ignore the current action and start
             # a new episode.
             return self.reset()
 
-        """ Gestion des actions légales """
-        legal_actions = generate_legal_vector(self._state)
+        reward = estimated_advantage(action, self._state.get_gamestate())
+        self._state = Orchestrator().tour_ia_from_training(self._state, action)
+        legal_actions = generate_legal_vector(self._state.get_gamestate())
+        obs = self.observation_spec()
+        obs['observation'] = np.array(itemgetter(*columns_actual_state)(self._state.get_gamestate()))
+        obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
 
-        if not(legal_actions[1] or legal_actions[2]):
-            reward = 0
+        while self._state.get_gamestate()['pseudo_j'] != 'IA1':
+            self._state = Orchestrator().tour_oldia_from_training(self._state, old_policy, oldpolicy_state)
+            if self._state.get_gamestate()['pv_adv'] <= 0:
+                reward = -1
+                self._episode_ended = True
+                return ts.termination(obs, reward)
+
+        """ Gestion des actions légales """
+        legal_actions = generate_legal_vector(self._state.get_gamestate())
+        obs = self.observation_spec()
+        obs['observation'] = np.array(itemgetter(*columns_actual_state)(self._state.get_gamestate()))
+        obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
+
+        if self._state.get_gamestate()['pv_adv'] <= 0:
             self._episode_ended = True
-            obs = self.observation_spec()
-            obs['observation'] = np.array(self._state, dtype=np.int32)
-            obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
             return ts.termination(obs, reward)
         else:
-            if int(action) == self._state['action']:
-                if self._state['victoire'] == 1:
-                    reward = 1
-                else:
-                    reward = -1
-                obs = self.observation_spec()
-                obs['observation'] = np.array(self._state, dtype=np.int32)
-                obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
-                if int(action) == 0:
-                    self._episode_ended = True
-                    return ts.termination(obs, reward)
-                else:
-                    return ts.transition(obs, reward)
-            else:
-                if self._state['victoire'] == 1:
-                    reward = -0.05
-                else:
-                    reward = 0.02
-                obs = self.observation_spec()
-                obs['observation'] = np.array(self._state, dtype=np.int32)
-                obs['valid_actions'] = np.array(legal_actions, dtype=np.bool_)
-                if int(action) == 0:
-                    self._episode_ended = True
-                    return ts.termination(obs, reward)
-                else:
-                    return ts.transition(obs, reward)
+            return ts.transition(obs, reward)
 
 
-
-X_train, X_test = train_test_split(df_state, test_size=0.2)
-train_env = Frenchstone(X_train.reset_index().drop('index', axis=1))
-eval_env = Frenchstone(X_test.reset_index().drop('index', axis=1))
+train_env = Frenchstone()
+eval_env = Frenchstone()
 train_env = tf_py_environment.TFPyEnvironment(train_env, check_dims=True)
 eval_env = tf_py_environment.TFPyEnvironment(eval_env, check_dims=True)
 time_step = train_env.reset()
 
-num_iterations = 10000  # @param {type:"integer"}
-initial_collect_steps = 10  # @param {type:"integer"}
-collect_steps_per_iteration = 1  # @param {type:"integer"}
-replay_buffer_max_length = 150000  # @param {type:"integer"}
+num_iterations = 150000  # @param {type:"integer"}
+initial_collect_steps = 1  # @param {type:"integer"}
+collect_steps_per_iteration = 30  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
 
 batch_size = 512  # @param {type:"integer"}
 learning_rate = 1e-4  # @param {type:"number"}
 lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate=1e-5,
+    initial_learning_rate=5e-5,
     decay_steps=5000,
-    decay_rate=0.95)
-log_interval = 500  # @param {type:"integer"}
+    decay_rate=0.9)
+epsilon = tf.compat.v1.train.polynomial_decay(
+            0.5,
+            tf.Variable(0, trainable=False),
+            150000 / ATARI_FRAME_SKIP / self._update_period,
+            end_learning_rate=epsilon_greedy)
+log_interval = 100  # @param {type:"integer"}
 
 num_eval_episodes = 100  # @param {type:"integer"}
-eval_interval = 1000  # @param {type:"integer"}
+eval_interval = 500  # @param {type:"integer"}
 
-replay_buffer_capacity = 150000  # @param {type:"integer"}
+replay_buffer_capacity = 100000  # @param {type:"integer"}
 
-fc_layer_params = (250, 100, 100, 32, 16)
+fc_layer_params = (512, 256, 128, 64, 32, 16)
 action_tensor_spec = tensor_spec.from_spec(train_env.action_spec())
 num_actions = action_tensor_spec.maximum - action_tensor_spec.minimum + 1
 
@@ -186,7 +234,7 @@ def observation_action_splitter(obs):
     return obs['observation'], obs['valid_actions']
 
 
-agent = dqn_agent.DqnAgent(
+agent = dqn_agent.DdqnAgent(
     train_env.time_step_spec(),
     train_env.action_spec(),
     q_network=q_net,
@@ -194,7 +242,7 @@ agent = dqn_agent.DqnAgent(
     td_errors_loss_fn=common.element_wise_squared_loss,
     train_step_counter=train_step_counter,
     observation_and_action_constraint_splitter=observation_action_splitter,
-    epsilon_greedy=0.02)
+    epsilon_greedy=0.2)
 
 agent.initialize()
 
@@ -202,13 +250,21 @@ agent.initialize()
 def compute_avg_return(environment, policy, num_episodes=10):
     total_return = 0.0
     for _ in range(num_episodes):
-        time_step = environment.reset()
+        timestep = environment.reset()
         episode_return = 0.0
 
-        while not time_step.is_last():
-            action_step = policy.action(time_step)
-            time_step = environment.step(action_step.action)
-            episode_return += time_step.reward
+        while not timestep.is_last():
+            # print('-----------------------------------------')
+            # print('-----------------------------------------')
+            # print('-----------------------------------------')
+            # print(timestep)
+            # print('-----------------------------------------')
+            action_step = policy.action(timestep)
+            # print(action_step)
+            timestep = environment.step(action_step.action)
+            # print('-----------------------------------------')
+            # print(timestep)
+            episode_return += timestep.reward
         total_return += episode_return
 
     avg_return = total_return / num_episodes
@@ -218,10 +274,19 @@ def compute_avg_return(environment, policy, num_episodes=10):
 random_policy = random_tf_policy.RandomTFPolicy(train_env.time_step_spec(),
                                                 train_env.action_spec())
 
+
+""" Relance du dernier modèle """
+# q_policy = tf.compat.v2.saved_model.load('frenchstone_agent_v0.02a')
+# policy_state = q_policy.get_initial_state(batch_size=512)
+"""-------------------------------------------------"""
+
+""" Entraînement d'un nouveau modèle """
 q_policy = q_policy.QPolicy(train_env.time_step_spec(),
                             train_env.action_spec(),
                             q_net,
                             observation_and_action_constraint_splitter=observation_action_splitter)
+"""-------------------------------------------------"""
+
 
 compute_avg_return(eval_env, q_policy, num_eval_episodes)
 
@@ -231,33 +296,20 @@ replay_buffer = tf_uniform_replay_buffer.TFUniformReplayBuffer(
     batch_size=train_env.batch_size,
     max_length=replay_buffer_capacity)
 
-time_step = train_env.current_time_step()
-action_step = q_policy.action(time_step)
-next_time_step = train_env.step(action_step.action)
-traj = trajectory.from_transition(time_step, action_step, next_time_step)
-# print(train_env.batched)
-# print(train_env.batch_size)
-# #
-# # print(agent.collect_data_spec)
-# # print(replay_buffer.data_spec)
-# # print(time_step)
-# # print(action_step)
-# # print(next_time_step)
-# print('----------------------------------------------------------------------')
-# print(is_batched_nested_tensors(traj.action, agent.collect_data_spec.action))
-# print(is_batched_nested_tensors(traj.discount, agent.collect_data_spec.discount))
-# print(is_batched_nested_tensors(traj.next_step_type, agent.collect_data_spec.next_step_type))
-# print(is_batched_nested_tensors(traj.observation['observation'], agent.collect_data_spec.observation['observation']))
-# print(is_batched_nested_tensors(traj.observation['valid_actions'], agent.collect_data_spec.observation['valid_actions']))
-# print(is_batched_nested_tensors(traj.reward, agent.collect_data_spec.reward))
-# print(is_batched_nested_tensors(traj.step_type, agent.collect_data_spec.step_type))
-
 
 def collect_step(environment, policy):
-    time_step = environment.current_time_step()
-    action_step = policy.action(time_step)
+    timestep = environment.current_time_step()
+    action_step = policy.action(timestep)
     next_time_step = environment.step(action_step.action)
-    traj = trajectory.from_transition(time_step, action_step, next_time_step)
+    # print(timestep)
+    # print('-------------------------------------------------')
+    # print(action_step)
+    # print('-------------------------------------------------')
+    # print(next_time_step)
+    # print('-------------------------------------------------')
+    # print('-------------------------------------------------')
+    # print('-------------------------------------------------')
+    traj = trajectory.from_transition(timestep, action_step, next_time_step)
 
     # Add trajectory to the replay buffer
     replay_buffer.add_batch(traj)
@@ -317,6 +369,6 @@ steps = range(0, num_iterations + 1, eval_interval)
 plt.plot(steps, returns)
 plt.ylabel('Average Return')
 plt.xlabel('Step')
-plt.ylim(bottom=-1)
-plt.ylim(top=3)
+plt.ylim(bottom=-1.1)
+plt.ylim(top=1.1)
 plt.show()
